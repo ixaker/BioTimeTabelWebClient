3683c5ea940c7b911d24d50539e0877ada1da865
Sushko Andriy <itsushkoandriy@gmail.com>
2024-04-12 11:04:42 +0300

    fix styles in table

--- src/components/Rodal/Modal.tsx
+++ src/components/Rodal/Modal.tsx
@@ -7,48 +7,48 @@ import CloseButton from './closeButton';
 
 const Modal: React.FC = () => {
 
-const { state, dispatch } = useAppContext();
-const { modal } = state;
-const { visible, data } = modal;
-const [showModal, setShowModal] = useState(false);
-console.log(data);
+  const { state, dispatch } = useAppContext();
+  const { modal } = state;
+  const { visible, data } = modal;
+  const [showModal, setShowModal] = useState(false);
+  console.log(data);
 
-useEffect(() => {
-  if (visible) {
-    setShowModal(false);
-    setTimeout(() => {
-      setShowModal(true);
-    }, 200);
-    
-  } else {
-    setShowModal(false);
-  }
+  useEffect(() => {
+    if (visible) {
+      setShowModal(false);
+      setTimeout(() => {
+        setShowModal(true);
+      }, 200);
+
+    } else {
+      setShowModal(false);
+    }
 
 
-}, [visible,data]);
+  }, [visible, data]);
 
-useEffect(() => {
-  setShowModal(visible); // Оновіть showModal при зміні visible
-}, [visible]);
+  useEffect(() => {
+    setShowModal(visible);
+  }, [visible]);
 
-useEffect(() => {
-  if (visible) {
-    const timer = setTimeout(() => {
-      onClose();
-    }, 60000); // 60000 мілісекунд = 1 хвилина
+  useEffect(() => {
+    if (visible) {
+      const timer = setTimeout(() => {
+        onClose();
+      }, 60000);
 
-    return () => clearTimeout(timer);
-  }
-}, [visible]);
+      return () => clearTimeout(timer);
+    }
+  }, [visible]);
 
   const onClose = () => {
-    dispatch({ type: 'SET_MODAL', payload: { visible: false,  data: modal.data} });
+    dispatch({ type: 'SET_MODAL', payload: { visible: false, data: modal.data } });
   };
 
   let customStyles = {};
-  
+
   if (data.error === true) {
-    customStyles = { 
+    customStyles = {
       border: "3px solid red",
       borderRadius: "10px",
       padding: "10px",
@@ -70,75 +70,75 @@ useEffect(() => {
       height: "200px",
     };
   }
-  
+
   return (
-    <Rodal 
-        visible={showModal} 
-        onClose={onClose} 
-        
-        closeOnEsc 
-        showCloseButton 
-        enterAnimation='zoom' 
-        showMask
-        customStyles={customStyles}
-        customMaskStyles={{
-          backgroundColor: 'rgba(0, 0, 0, 0.5)',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
+    <Rodal
+      visible={showModal}
+      onClose={onClose}
+
+      closeOnEsc
+      showCloseButton
+      enterAnimation='zoom'
+      showMask
+      customStyles={customStyles}
+      customMaskStyles={{
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
     >
       <div style={{
         border: "0px solid #ccc",
         borderRadius: "5px",
         padding: "10px",
         width: "95%",
-        
+
       }}>
-        <div style={{ 
-          display: "flex", 
-          justifyContent: "center", 
-          alignItems: "center", 
-          marginBottom: "10px", 
+        <div style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          marginBottom: "10px",
         }}>
-          <span style={{ 
-            fontWeight: "bold", 
-            marginRight: "0px", 
+          <span style={{
+            fontWeight: "bold",
+            marginRight: "0px",
             fontSize: "30px",
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
           }}>{data.state} -  {data.time}</span>
-          
+
         </div>
-        <p style={{ 
+        <p style={{
           margin: 0,
           marginBottom: "5px",
           fontSize: "30px",
-          }}>{data.first_name}</p>
-          <p
-            style={{
-              color: "rgb(141, 4, 4)",
-              fontSize: "17px",
-              fontWeight: "bold",
-            }}
-          >
-            {data.msg}
-          </p>
-          <button onClick={
-            () => {
-              console.log("Button clicked!");
-              const newDataForModal = {
-                first_name: 'Другий модал',
-                time: 'asdf',
-                state: 'asdfasdf',
-                error: true,
-                msg: 'asdfsdf',
+        }}>{data.first_name}</p>
+        <p
+          style={{
+            color: "rgb(141, 4, 4)",
+            fontSize: "17px",
+            fontWeight: "bold",
+          }}
+        >
+          {data.msg}
+        </p>
+        {/* <button onClick={
+          () => {
+            console.log("Button clicked!");
+            const newDataForModal = {
+              first_name: 'Другий модал',
+              time: 'asdf',
+              state: 'asdfasdf',
+              error: true,
+              msg: 'asdfsdf',
             };
-              dispatch({ type: 'SET_MODAL', payload: { visible: true, data: newDataForModal } });
-            }
-          }>Click me!</button>
-      <CloseButton />
+            dispatch({ type: 'SET_MODAL', payload: { visible: true, data: newDataForModal } });
+          }
+        }>Click me!</button> */}
+        <CloseButton />
       </div>
     </Rodal>
   );
