e48aaeffe8ac5ad2d2a932d2b274caf8f4d70e76
Sushko Andriy <itsushkoandriy@gmail.com>
2024-04-15 13:15:17 +0300

    fix click with add style

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,5 +1,6 @@
+import { useState } from 'react'
 import './slider.css'
-
+import 'animate.css/animate.min.css';
 
 interface SliderProps {
     handleSliderClick: (update: 'increase' | 'decrease') => void;
@@ -7,31 +8,34 @@ interface SliderProps {
   }
 
 const Slider: React.FC<SliderProps>= ({ handleSliderClick, date }) => {
-    
+    const [isLeftArrowActive, setIsLeftArrowActive] = useState(false);
+    const [isRightArrowActive, setIsRightArrowActive] = useState(false);
     
     
     const handleLeftArrowClick = () => {
         console.log('handleLeftArrowClick');
-        
-        handleSliderClick('decrease') 
+        setIsLeftArrowActive(true);
+        handleSliderClick('decrease')
+        setTimeout(() => setIsLeftArrowActive(false), 300); 
          };
     
       const handleRightArrowClick = () => {
         console.log('handleRightArrowClick');
-        
+        setIsRightArrowActive(true);
         handleSliderClick('increase')
+        setTimeout(() => setIsRightArrowActive(false), 300);
       };
 
     return (
     <>
         <div className="slider-container">
-            <div>
+            <div className={`arrows-container ${isLeftArrowActive ? 'clicked' : ''}`}>
                 <i className="bi bi-arrow-left-circle btnArrow slider-arrows" onClick={handleLeftArrowClick}></i>
             </div>
             <div>
                 <span className="date-input slider-input">{date}</span>
             </div>
-            <div>
+            <div className={`arrows-container ${isRightArrowActive ? 'clicked' : ''}`}>
                 <i className="bi bi-arrow-right-circle btnArrow slider-arrows" onClick={handleRightArrowClick}></i>
             </div>
         </div>
