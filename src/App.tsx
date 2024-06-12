e93a0ac7f6825dc781aef71e0693ac55b7bcf7e1
itSuhsko14 <itsushkoandriy@gmail.com>
2024-06-12 10:53:31 +0300

    refactor App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,28 @@
-import { useState, useMemo } from 'react'
-import './App.css'
-import Slider from './components/Slider/Slider'
-import Table from './components/Table/Table'
-import WebSocket from './components/WebSocket/WebSocket'
-import { getCurrentDate, formatDate, increaseDate, decreaseDate } from './components/utils/dateUtils'
-import Modal from './components/Rodal/Modal'
-import Loader from './components/Loader/Loader'
-import LoaderDisconect from './components/Loader/LoaderDisconect'
-import DataLoadingComponent from './components/DataLoadingComponent/DataLoadingComponent'
+import { useState, useMemo } from "react";
+import "./App.css";
+import WebSocket from "./components/WebSocket/WebSocket";
+import Slider from "./components/Slider/Slider";
+import Table from "./components/Table/Table";
+import Modal from "./components/Rodal/Modal";
+import Loader from "./components/Loader/Loader";
+import LoaderDisconect from "./components/Loader/LoaderDisconect";
+import FullScreen from "./components/FullScreen/FullScreen";
+
+import { getCurrentDate, formatDate, increaseDate, decreaseDate } from "./components/utils/dateUtils";
+import DataLoadingComponent from "./components/DataLoadingComponent/DataLoadingComponent";
 // import ScreenWakeLock from './components/WakeLock/WakeLock'
-import FullScreen from './components/FullScreen/FullScreen'
 
 function App() {
   const [date, setDate] = useState(getCurrentDate());
-  // const [isDataLoaded, setIsDataLoaded] = useState(true);
   const [isSocketDisconnect, setIsSocketDisconnect] = useState(false);
   const [selectedId, setSelectedId] = useState<number | null>(null);
-  // const memoizedState = useMemo(() => ({ date, isDataLoaded }), [date, isDataLoaded]);
   const memoizedDate = useMemo(() => formatDate(date), [date]);
   const isDataLoaded = DataLoadingComponent();
-  console.log('App start');
-  
-  // useEffect(() => {
-  //   if (state.data.length > 0) {
-  //     setIsDataLoaded(true);
-  //   }
-  // }, [state.data]);
+  console.log("App start");
 
-  const handleSliderClick = (update: 'increase' | 'decrease') => {
-    if (update === 'increase') {
-      setDate(increaseDate(date))
+  const handleSliderClick = (update: "increase" | "decrease") => {
+    if (update === "increase") {
+      setDate(increaseDate(date));
     } else {
       setDate(decreaseDate(date));
     }
@@ -45,30 +38,20 @@ function App() {
         setSelectedId={setSelectedId}
       />
       {/* <ScreenWakeLock/> */}
-      {isDataLoaded
-        ? <>
-          <Slider
-            handleSliderClick={handleSliderClick} 
-            date={memoizedDate} 
-          />
-          <Table
-            selectedId={selectedId}
-          />
+      {isDataLoaded ? (
+        <>
+          <Slider handleSliderClick={handleSliderClick} date={memoizedDate} />
+          <Table selectedId={selectedId} />
           <Modal />
           <FullScreen />
         </>
-        : <Loader />
-      }
+      ) : (
+        <Loader />
+      )}
 
-      {isSocketDisconnect
-        ? <LoaderDisconect />
-        : null
-      }
-
+      {isSocketDisconnect ? <LoaderDisconect /> : null}
     </>
-  )
+  );
 }
 
-
-export default App
-
+export default App;
