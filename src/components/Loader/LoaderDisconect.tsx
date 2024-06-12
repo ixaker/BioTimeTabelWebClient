b7365e305cd4adf74ff911133b35cab862dc5006
itSuhsko14 <itsushkoandriy@gmail.com>
2024-06-12 10:48:51 +0300

    fix styles

--- src/components/Loader/LoaderDisconect.tsx
+++ src/components/Loader/LoaderDisconect.tsx
@@ -1,34 +1,31 @@
 export default function LoaderDisconect() {
   return (
-    <div style={{
-        position: 'fixed',
+    <div
+      style={{
+        position: "fixed",
         bottom: "20px",
         right: "20px",
-        width: '80%',
-        height: '10%',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'rgba(0, 0, 0, 0.3)',
-        borderRadius: '30px',
+        width: "80%",
+        height: "10%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "rgba(0, 0, 0, 0.3)",
+        borderRadius: "30px",
         zIndex: 9999,
-        
-    }}>
-        
-        <div 
-            className="loader"
-            style={{
-                
-                fontSize: '40px',
-                fontWeight: '600',
-                color: 'white',
-                textShadow: '2px 2px 2px #000'
-                
-            }}
-        >
-            Зв'язок з севером втрачено ...
-    
-        </div>
+      }}
+    >
+      <div
+        className="loader"
+        style={{
+          fontSize: "5vw",
+          fontWeight: "600",
+          color: "white",
+          textShadow: "2px 2px 2px #000",
+        }}
+      >
+        Зв'язок з севером втрачено ...
+      </div>
     </div>
-  )
+  );
 }
